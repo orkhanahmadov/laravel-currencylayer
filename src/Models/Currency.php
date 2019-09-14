a3f6306ee645f4cb7a1a6a56df553f777d4d3f38
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-09-14 23:26:58 +0200

    WIP

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -2,6 +2,7 @@
 
 namespace Orkhanahmadov\LaravelCurrencylayer\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -22,5 +23,26 @@ class Currency extends Model
     public function rates(): HasMany
     {
         return $this->hasMany(Rate::class, 'source_currency_id');
+    }
+
+    /**
+     * @param Currency|string $currency
+     * @param Carbon|string|null $date
+     *
+     * @return Rate|null
+     */
+    public function rateFor($currency, $date = null): ?Rate
+    {
+        if (! $currency instanceof Currency) {
+            $currency = Currency::where('code', $currency)->firstOrFail();
+        }
+
+        $query = $this->rates()->where('target_currency_id', $currency->id);
+
+        if ($date) {
+            $query = $query->whereDay('timestamp', $date instanceof Carbon ? $date : Carbon::parse($date));
+        }
+
+        return $query->orderByDesc('timestamp')->first();
     }
 }
--- tests/Models/CurrencyTest.php
+++ tests/Models/CurrencyTest.php
@@ -2,6 +2,7 @@
 
 namespace Orkhanahmadov\LaravelCurrencylayer\Tests\Models;
 
+use Carbon\Carbon;
 use Orkhanahmadov\LaravelCurrencylayer\Models\Rate;
 use Orkhanahmadov\LaravelCurrencylayer\Tests\TestCase;
 use Orkhanahmadov\LaravelCurrencylayer\Models\Currency;
@@ -15,5 +16,48 @@ class CurrencyTest extends TestCase
 
         $this->assertCount(2, $currency->rates);
         $this->assertInstanceOf(Rate::class, $currency->rates->first());
+    }
+
+    public function testRateFor()
+    {
+        $sourceCurrency = factory(Currency::class)->create();
+        $targetCurrency = factory(Currency::class)->create();
+        $createdRate = factory(Rate::class)->create([
+            'source_currency_id' => $sourceCurrency->id,
+            'target_currency_id' => $targetCurrency->id,
+            'timestamp' => '2019-01-20 15:20:30',
+        ]);
+        factory(Rate::class)->create([
+            'source_currency_id' => $sourceCurrency->id,
+            'target_currency_id' => $targetCurrency->id,
+            'timestamp' => '2019-01-19 15:20:30',
+        ]);
+
+        $rate = $sourceCurrency->rateFor($targetCurrency->code);
+        $this->assertSame($createdRate->rate, $rate->rate);
+    }
+
+    public function testRateForWithDate()
+    {
+        $sourceCurrency = factory(Currency::class)->create();
+        $targetCurrency = factory(Currency::class)->create();
+        $createdRate = factory(Rate::class)->create([
+            'source_currency_id' => $sourceCurrency->id,
+            'target_currency_id' => $targetCurrency->id,
+            'timestamp' => '2019-01-20 15:20:30',
+        ]);
+        factory(Rate::class)->create([
+            'source_currency_id' => $sourceCurrency->id,
+            'target_currency_id' => $targetCurrency->id,
+            'timestamp' => '2019-01-20 12:20:30',
+        ]);
+        factory(Rate::class)->create([
+            'source_currency_id' => $sourceCurrency->id,
+            'target_currency_id' => $targetCurrency->id,
+            'timestamp' => '2019-01-22 18:20:30',
+        ]);
+
+        $rate = $sourceCurrency->rateFor($targetCurrency, '2019-01-20');
+        $this->assertSame($createdRate->rate, $rate->rate);
     }
 }
