2088b3051af554448b3c05dac16f4d5f17211252
Orkhan Ahmadov <ahmadov90@gmail.com>
2019-09-16 15:22:21 +0000

    Apply fixes from StyleCI

--- src/Currencylayer.php
+++ src/Currencylayer.php
@@ -5,8 +5,8 @@ namespace Orkhanahmadov\LaravelCurrencylayer;
 use Carbon\Carbon;
 use Illuminate\Support\Arr;
 use OceanApplications\currencylayer\client;
-use Orkhanahmadov\LaravelCurrencylayer\Contracts\CurrencyService;
 use Orkhanahmadov\LaravelCurrencylayer\Models\Currency;
+use Orkhanahmadov\LaravelCurrencylayer\Contracts\CurrencyService;
 
 class Currencylayer implements CurrencyService
 {
